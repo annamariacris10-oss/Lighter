e34d80216676b0d6ae3f5b4e52183bc4f498b6d7
annamariacris10-oss <annamariacris10@gmail.com>
2025-12-05 16:37:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("Lighter", divider="rainbow")
+st.subheader("This a a music controlled light show generator using arduino ")
+
+st.markdown('''
+   this is a test for this
+''')
